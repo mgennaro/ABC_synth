Fixed a couple of problems with the likelihood normalization. Introduced a bounded Gaussian and bounded uniform kernel to deal with the completeness limit. Modified MCMC_PPP_simul to log a bit more info for postprocessing

analysis/MCMC_PPP_simul.py
@@ -20,26 +20,33 @@ from sim_utils.GeneralRandom import GeneralRandom
 from sim_utils.getmags import get_absolute_mags,get_observable_mags,get_noisy_mags
 from sim_utils.oneCMD import oneCMD
 
-from analysis.PPP_loglik import loglik,GKDE_lik,set_GR,logpost,logpost_nb
+from analysis.PPP_loglik import loglik,GKDE_lik,set_GR,logpost,logpost_nb,TGKDE_lik,UKDE_lik
 
 ################################
 #Initial setup
 
 root = '/user/gennaro/ABC_synth/WORK/herc_simul/'
 
-case = 'fp'  # suffix for the output files
+case = 'trg_sk'  # suffix for the output files
 
-tru_N, tru_alpha, tru_bf = 2500, -2.0, 0.3
+tru_N, tru_alpha, tru_bf = 2500, -2.0, 0.0
 
-fix_bf, fix_bfval = False, tru_bf # If fix_bf true do not fit for binary fraction but keep it equal to  fix_bfval
+fix_bf, fix_bfval = True, tru_bf # If fix_bf true do not fit for binary fraction but keep it equal to  fix_bfval
 
-ra_min,ra_max = -3, -1 # Range for the prior in alpha (i.e. range where the uniform prior on alpha extends)
-rb_min,rb_max = 0., 1  #  Range for the prior in bf (i.e. range where the uniform prior on bf extends)
-                    
+ra_min,ra_max = -4, -0.5 # Range for the prior in alpha (i.e. range where the uniform prior on alpha extends)
+rb_min,rb_max = 0., 1.  #  Range for the prior in bf (i.e. range where the uniform prior on bf extends)
+
+#Limits on the catalogs                    
+mag2lim = 28.25
+
+#Width of the magnitude and color kernels
+
+bwcol,bwmag = 0.01,0.025
+
 #emcee parameters
 nthreads = 10
-nwalkers = 150
-nsteps = 500
+nwalkers = 200
+nsteps = 1000
 
 ################################
 
@@ -59,7 +66,7 @@ AS_mag2_out = AScat['AS_mag2_out']
 AS_det      = AScat['AS_det']
 
 # Limit the detections to brigther than a certain magnitude
-AS_det = AS_det & (AS_mag1_out < 28.25)
+AS_det = AS_det & (AS_mag2_out < mag2lim)
 
 #############################
 # Fixed values of Distance modulus and exinction
@@ -148,8 +155,9 @@ dictopass ={'GRdic':GRdic,
             'AS_det':AS_det,
             'data':[],
             'ndata':[],
-            'bwcol':0.02,
-            'bwmag':0.05,
+            'bwcol':bwcol,
+            'bwmag':bwmag,
+            'mag2lim':mag2lim,
             'hp_alpha': hpa_GR,
             'hp_bf':hpb_GR
             }
@@ -159,9 +167,9 @@ dictopass ={'GRdic':GRdic,
 
 nsimdata = tru_N
 mss_GR_h, bin_GR_h = set_GR(tru_alpha,tru_bf)
-kwargs = copy.deepcopy(dictopass)
+kwargsh = copy.deepcopy(dictopass)
 
-GRdic_h = kwargs['GRdic']
+GRdic_h = kwargsh['GRdic']
 GRdic_h['logM'] = mss_GR_h
 GRdic_h['BinQ'] = bin_GR_h
 
@@ -171,8 +179,8 @@ print('Generating fake catalog')
 sys.stdout.flush()
 
 pars, absmags, obsmags, simdata, nall, indGR = oneCMD(nsimdata,GRdic_h,
-                                                       kwargs['iso_int'],kwargs['ASKDtree'],kwargs['AS_mag1_in'],kwargs['AS_mag2_in'],
-                                                       kwargs['AS_mag1_out'],kwargs['AS_mag2_out'],kwargs['AS_det'],verbose=True)
+                                                       kwargsh['iso_int'],kwargsh['ASKDtree'],kwargsh['AS_mag1_in'],kwargsh['AS_mag2_in'],
+                                                       kwargsh['AS_mag1_out'],kwargsh['AS_mag2_out'],kwargsh['AS_det'],verbose=True)
 
 dic = {'pars':pars,
        'absmags':absmags,
@@ -186,7 +194,7 @@ dic = {'pars':pars,
 with bz2.BZ2File(root+'/Results/simcat_'+case+'.pbz2', 'w') as f:
     pickle.dump(dic,f)
 
-print('Time elapsed',time.time()-t0)
+print('Time elapsed (sec)',time.time()-t0)
 sys.stdout.flush()
 
 dictopass['data'] = simdata
@@ -234,10 +242,10 @@ for i, result in enumerate(sampler.sample(p0, iterations=nsteps)):
     np.savetxt(f,np.column_stack((result[0],result[1])))
     f.close()
         
-print('Total time',time.time()-tt1)
+print('Total time (hours)',(time.time()-tt1)/3600.)
 sys.stdout.flush()
 
-print('Saving results')
+print('Saving results on:'+root+'/Results/chain_'+case+'.pbz2')
 sys.stdout.flush()
 
 dic = {'a':sampler.a,
@@ -245,6 +253,15 @@ dic = {'a':sampler.a,
        'chain':sampler.chain,
        'lnprobability' : sampler.lnprobability,
        'naccepted': sampler.naccepted,
+       'bwcol':bwcol,
+       'bwmag':bwmag,
+       'mag2lim':mag2lim,
+       'fix_bf':fix_bf,
+       'fix_bfval':fix_bfval,
+       'ra_min':ra_min,
+       'ra_max':ra_max,
+       'rb_min':rb_min,
+       'rb_max':rb_max  
        }
 
 with bz2.BZ2File(root+'/Results/chain_'+case+'.pbz2', 'w') as f:

analysis/PPP_loglik.py
@@ -9,7 +9,8 @@ from sim_utils.getmags import get_absolute_mags,get_observable_mags,get_noisy_ma
 from sim_utils.oneCMD import oneCMD
 import copy,time,sys
 from sklearn.neighbors import KDTree
-
+from scipy.special import erfc
+from scipy.stats import norm
 
 def set_GR(alpha,bf):
 
@@ -45,7 +46,77 @@ def GKDE_lik(noisymags,data,bwcol,bwmag):
     indlnlik     = np.log(np.sum(exponentials,axis=1))
     alllnlik     = np.sum(indlnlik)
 
-    return -ndata*(np.log(nmodel*2*np.pi) + 2*np.log(bwcol*bwmag)) +alllnlik
+    return -ndata*(np.log(nmodel*2*np.pi*bwcol*bwmag)) +alllnlik
+
+def TGKDE_lik(noisymags,data,bwcol,bwmag,truncmag):
+
+    colmod = noisymags[:,0]-noisymags[:,1]
+    magmod = noisymags[:,1]
+    coldat = data[:,0]-data[:,1]
+    magdat = data[:,1]
+
+    ndata  = magdat.size
+    nmodel = magmod.size
+
+    exponents = np.zeros([ndata,nmodel])
+       
+    for i in range(ndata):
+        dx2 = ((colmod-coldat[i])/bwcol)**2
+        dy2 = ((magmod-magdat[i])/bwmag)**2
+        exponents[i,:] =  -0.5*(dx2+dy2)
+
+
+    nn = norm(loc=0,scale=bwmag)
+    dy = magmod - truncmag
+    kfact = 1./(1-nn.cdf(dy))
+
+    exponentials = kfact*np.exp(exponents)
+    indlnlik     = np.log(np.sum(exponentials,axis=1))
+    alllnlik     = np.sum(indlnlik)
+
+    if (np.isnan(alllnlik) == True):
+        dic = {'noisymags':noisymags,
+               'data':data,
+               'bwcol':bwcol,
+               'bwmag':bwmag,
+               'truncmag':truncmag,
+               'dy':dy,
+               'kfact':kfact,
+               'exponents':exponents
+               }
+        with bz2.BZ2File('/user/gennaro/ABC_synth/WORK/herc_simul/Results/likerr.pbz2', 'w') as f:
+            pickle.dump(dic,f)
+
+
+    return -ndata*(np.log(nmodel*2*np.pi*bwcol*bwmag)) +alllnlik
+
+
+def UKDE_lik(noisymags,data,bwcol,bwmag,truncmag):
+
+    colmod = noisymags[:,0]-noisymags[:,1]
+    magmod = noisymags[:,1]
+    coldat = data[:,0]-data[:,1]
+    magdat = data[:,1]
+
+    ndata  = magdat.size
+    nmodel = magmod.size
+
+    dy   = truncmag-magmod
+    norm = np.where( dy > 0.5 *bwmag,np.repeat(1./(bwcol*bwmag),nmodel), 1/(bwcol*(0.5*bwmag+dy))) / nmodel
+    lkl  = 0.
+    
+    for i in range(ndata):
+        im  = np.abs(magdat[i]-magmod) < bwmag
+        ic  = np.abs(coldat[i]-colmod) < bwcol
+        iok = im & ic
+        if (np.any(iok) == True):
+            lkl += np.log(np.sum(norm[iok]))
+        else:
+            lkl = -np.inf
+            break
+
+    return lkl
+
 
 def loglik(nsim,alpha,bf,**kwargs):
 
@@ -69,8 +140,22 @@ def loglik(nsim,alpha,bf,**kwargs):
     '''
     Compute the likelihood using a gaussian kde
     '''
+#    lnlik = GKDE_lik(noisymags,kwargs['data'],kwargs['bwcol'],kwargs['bwmag'])
 
-    lnlik = GKDE_lik(noisymags,kwargs['data'],kwargs['bwcol'],kwargs['bwmag'])
+    '''
+    Compute the likelihood using a bounded gaussian kde
+    '''
+    lnlik = TGKDE_lik(noisymags,kwargs['data'],kwargs['bwcol'],kwargs['bwmag'],kwargs['mag2lim'])
+
+
+    '''
+    Compute the likelihood using a bounded uniform kde
+    '''
+#    lnlik = UKDE_lik(noisymags,kwargs['data'],kwargs['bwcol'],kwargs['bwmag'],kwargs['mag2lim'])
+
+    '''
+    Add normalization (KDE returns probability not intenisty of PPP
+    '''
     lnlik = -nsim+lnlik+kwargs['ndata']*np.log(nsim)
 
     return lnlik